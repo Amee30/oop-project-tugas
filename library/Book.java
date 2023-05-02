fa0c7dd1218b61fa52134452ce12312ce58ad561
Ame <darmaputra2004@gmail.com>
2023-05-02 21:02:49 +0800

    Minor Updates
    on Book class now using record, (ide recomendation)

--- library/Book.java
+++ library/Book.java
@@ -1,18 +1,2 @@
-class Book {
-  private final String id;
-  private final String title;
-
-  public Book(String title, String id){
-    this.title = title;
-    this.id = id;
-  }
-
-
-  public String title(){
-    return title;
-  }
-
-  public String id(){
-    return id;
-  }
+record Book(String title, String id) {
 }
